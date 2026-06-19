e1f27be07a42484537e49681a8650ce74b5cd87c
JulienTH37 <julienth37@users.noreply.github.com>
2026-06-19 14:38:07 +0200

    Update Docusaurus configuration for new site URL
    config: serve docs under /doc/ on globe domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://repair-cafe-tours.github.io",
+  url: "https://globe.repaircafetours.fr",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Documentation/",
+  baseUrl: "/doc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Repair Café Tours",
-  projectName: "Documentation",
+  organizationName: "repaircafetours",
+  projectName: "GLOBE-documentation",
 
   onBrokenLinks: "throw",
 
